7e758e891383d7bb9a6b4969120ddbcc932e598b
Amit Thakur <at7129652@gmail.com>
2022-01-07 10:23:32 +0530

    created Backend

---
+++ Backend/schemas/save.js
@@ -0,0 +1,17 @@
+export default {
+    name: 'save',
+    title: 'Save',
+    type: 'document',
+    fields: [
+        {
+            name: 'postedBy',
+            name: 'PostedBy',
+            type: 'postedBy'
+        },
+        {
+            name: 'userId',
+            name: 'UserId',
+            type: 'string'
+        },
+    ]
+}

16ddb392f4e1f7c8be2aea5c8b20ad7550386570
Amit Thakur <at7129652@gmail.com>
2022-01-09 21:08:03 +0530

    added query for fetching posts for a specific category

--- Frontend/SharemeApp/src/components/Feed.jsx
+++ Frontend/SharemeApp/src/components/Feed.jsx
@@ -1,6 +1,34 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom';
+
+import { client } from '../client';
+import { searchQuery } from '../utils/data';
+import MasonryLayout from "./MasonryLayout";
+import Spinner from "./Spinner";
 
 const Feed = () => {
+    const [loading, setLoading] = useState(false);
+    const [pins, setPins] = useState(null);
+    const { categoryId } = useParams();
+
+    useEffect(() => {
+        setLoading(true);
+        if(categoryId) {
+            // fetching post for a specific category using categoryId
+            const query = searchQuery(categoryId);
+
+            client.fetch(query)
+                .then((data) => {
+                    setPins(data);
+                    setLoading(false);
+                })
+        } else {
+
+        }
+    }, [categoryId])
+
+    if(loading) return <Spinner message="We are adding new ideas to your feed!" />
+
     return (
         <div>
             feed
---
+++ Frontend/SharemeApp/src/components/MasonryLayout.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const MasonryLayout = () => {
+    return (
+        <div>
+            MasonryLayout
+        </div>
+    )
+}
+
+export default MasonryLayout

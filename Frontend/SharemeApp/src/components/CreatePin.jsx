a6b89cc61f94339dab97de695934d032752954e0
Amit Thakur <at7129652@gmail.com>
2022-01-09 20:31:44 +0530

    completed the navbar and added search bar

---
+++ Frontend/SharemeApp/src/components/CreatePin.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CreatePin = () => {
+    return (
+        <div>
+            creaePin
+        </div>
+    )
+}
+
+export default CreatePin;
---
+++ Frontend/SharemeApp/src/components/PinDetail.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const PinDetail = () => {
+    return (
+        <div>
+            pindetail
+        </div>
+    )
+}
+
+export default PinDetail;

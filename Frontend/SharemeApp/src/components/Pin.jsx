f78d8824d8cc4a64c2804080c4d23aedf4c85224
Amit Thakur <at7129652@gmail.com>
2022-01-10 14:54:55 +0530

    completed the Pin component

--- Frontend/SharemeApp/src/components/Pin.jsx
+++ Frontend/SharemeApp/src/components/Pin.jsx
@@ -6,11 +6,45 @@ import { AiTwotoneDelete } from 'react-icons/ai';
 import { BsFillArrowUpRightCircleFill } from 'react-icons/bs';
 
 import { client, urlFor } from '../client'
+import { fetchUser } from '../utils/fetchUser';
 
-const Pin = ({ pin: { postedBy, image, _id, destination }}) => {
+const Pin = ({ pin: { postedBy, image, _id, destination, save } }) => {
     const [postHovered, setPostHovered] = useState(false);
-    const [savingPost, setSavingPost] = useState(false);
-    const navigate = useNavigate(); 
+    const navigate = useNavigate();
+    const user = fetchUser();
+
+    console.log(save);
+
+    // if(length is >= 1 then it return true otherwise false)
+    const aleradySaved = !!(save?.filter((item) => item.postedBy._id === user.googleId))?.length;
+
+    const savePin = (id) => {
+        if (!aleradySaved) {
+            client
+                .patch(id)
+                .setIfMissing({ save: [] })
+                .insert('after', 'save[-1]', [{
+                    _key: uuidv4(),
+                    userId: user.googleId,
+                    postedBy: {
+                        _type: 'postedBy',
+                        _ref: user.googleId
+                    }
+                }])
+                .commit()
+                .then(() => {
+                    window.location.reload();
+                })
+        }
+    }
+
+    const deletePin = (id) => {
+        client
+            .delete(id)
+            .then(() => {
+                window.location.reload();
+            })
+    }
 
     return (
         <div className="m-2">
@@ -26,7 +60,7 @@ const Pin = ({ pin: { postedBy, image, _id, destination }}) => {
                         className="absolute top-0 w-full h-full flex flex-col justify-between p-1 pr-2 pt2 pb-2 z-50"
                         style={{ height: '100%' }}
                     >
-                        <divc className="flex items-center justify-between">
+                        <div className="flex items-center justify-between">
                             <div className="flex gap-2">
                                 <a
                                     href={`${image?.asset?.url}?dl=`}
@@ -37,10 +71,57 @@ const Pin = ({ pin: { postedBy, image, _id, destination }}) => {
                                     <MdDownloadForOffline />
                                 </a>
                             </div>
-                        </divc>
+                            {aleradySaved ? (
+                                <button type='button' className="bg-red-500 opacity-80 hover:opacity-100 text-white font-bold px-5 py-1 text-base rounded-3xl hover:shadow-md outline-none">
+                                    {save?.length} Saved
+                                </button>
+                            ) : (
+                                <button
+                                    onClick={(e) => {
+                                        e.stopPropagation();  // prevent from redirection of current page
+                                        savePin(_id);
+                                    }}
+                                    type='button' className="bg-red-500 opacity-80 hover:opacity-100 text-white font-bold px-5 py-1 text-base rounded-3xl hover:shadow-md outline-none">
+                                    Save
+                                </button>
+                            )}
+                        </div>
+                        <div className='flex justify-between items-center gap-2 w-full'>
+                            {destination && (
+                                <a
+                                    href={destination}
+                                    target="_blank"
+                                    rel="noreferrer"
+                                    className='bg-white flex items-center gap-2 text-black font-bold p-2 pl-4 pr-4 rounded-full opacity-70 hover:100 hover:shadow-md'
+                                >
+                                    <BsFillArrowUpRightCircleFill />
+                                    {destination.length > 20 ? destination.slice(8, 20) : destination.slice(8)}
+                                </a>
+                            )}
+                            {postedBy?._id === user.googleId && (
+                                <button
+                                    type='button'
+                                    onClick={(e) => {
+                                        e.stopPropagation();
+                                        deletePin(_id);
+                                    }}
+                                    className="bg-white p-2 rounded-full w-8 h-8 flex items-center justify-center text-dark opacity-75 hover:opacity-100 outline-none"
+                                >
+                                    <AiTwotoneDelete />
+                                </button>
+                            )}
+                        </div>
                     </div>
                 )}
             </div>
+            <Link to={`user-profile/${postedBy?._id}`} className='flex gap-2 mt-2 items-center'>
+                <img 
+                    className='w-8 h-8 rounded-full object-cover'
+                    src={postedBy?.image}
+                    alt="user-profile"
+                />
+                <p className='font-semibold capitalize'>{postedBy?.userName}</p>
+            </Link>
         </div>
     )
 }
--- Frontend/SharemeApp/src/container/Home.jsx
+++ Frontend/SharemeApp/src/container/Home.jsx
@@ -8,14 +8,13 @@ import Pins from "./Pins";
 import { userQuery } from "../utils/data";
 import { client } from "../client";
 import logo from "../assets/logo.png";
+import { fetchUser } from "../utils/fetchUser";
 
 const Home = () => {
     const [toggleSidebar, setToggleSidebar] = useState(false);
     const [user, setUser] = useState(null);
     const scrollRef = useRef(null);
-    const userInfo = localStorage.getItem("user") !== "undefined"
-        ? JSON.parse(localStorage.getItem("user"))
-        : localStorage.clear();
+    const userInfo = fetchUser();
 
     useEffect(() => {
         const query = userQuery(userInfo?.googleId);

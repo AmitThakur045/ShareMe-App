f85881e273fe709db2d1bb47b6757e5bb3e100cd
Amit Thakur <at7129652@gmail.com>
2022-01-11 11:58:03 +0530

    created Pin Detail component and add comments feature

--- Frontend/SharemeApp/src/components/Pin.jsx
+++ Frontend/SharemeApp/src/components/Pin.jsx
@@ -95,7 +95,7 @@ const Pin = ({ pin: { postedBy, image, _id, destination, save } }) => {
                                     className='bg-white flex items-center gap-2 text-black font-bold p-2 pl-4 pr-4 rounded-full opacity-70 hover:100 hover:shadow-md'
                                 >
                                     <BsFillArrowUpRightCircleFill />
-                                    {destination.length > 20 ? destination.slice(8, 20) : destination.slice(8)}
+                                    {destination.length > 15 ? `${destination.slice(8, 15)}...` : destination.slice(8)}
                                 </a>
                             )}
                             {postedBy?._id === user.googleId && (
--- Frontend/SharemeApp/src/utils/data.js
+++ Frontend/SharemeApp/src/utils/data.js
@@ -45,63 +45,174 @@ export const categories = [
       image: 'https://i.pinimg.com/236x/1b/c8/30/1bc83077e363db1a394bf6a64b071e9f.jpg',
     },
     {
-      name: 'other',
+      name: 'others',
       image: 'https://i.pinimg.com/236x/2e/63/c8/2e63c82dfd49aca8dccf9de3f57e8588.jpg',
     },
   ];
-
-export const userQuery = (userId) => {
-    const query = `*[_type == "user" && _id == '${userId}']`;
-
-    return query;
-}
-
-export const searchQuery = (searchTerm) => {
-    const query = `*[_type == "pin" && title match '${searchTerm}*' || category match '${searchTerm}*' || about match '${searchTerm}*']{
-        image {
-            asset -> {
-                url
-            }
-        },
-        _id, 
+  
+  export const feedQuery = `*[_type == "pin"] | order(_createdAt desc) {
+    image{
+      asset->{
+        url
+      }
+    },
+        _id,
         destination,
-        postedBy -> {
+        postedBy->{
+          _id,
+          userName,
+          image
+        },
+        save[]{
+          _key,
+          postedBy->{
             _id,
             userName,
             image
+          },
         },
-        save[] {
-            _key,
-            postedBy -> {
-                _id,
-                userName,
-                image
-            },
-        },
-    }`
-
-    return query;
-}
-
-export const feedQuery = `*[_type == 'pin'] | order(_createAt desc) {
-    image {
-        asset -> {
-            url
+      } `;
+  
+  export const pinDetailQuery = (pinId) => {
+    const query = `*[_type == "pin" && _id == '${pinId}']{
+      image{
+        asset->{
+          url
         }
-    },
-    _id, 
-    destination,
-    postedBy -> {
+      },
+      _id,
+      title, 
+      about,
+      category,
+      destination,
+      postedBy->{
         _id,
         userName,
         image
-    },
-    save[] {
-        _key,
-        postedBy -> {
-            _id,
-            userName,
-            image
+      },
+     save[]{
+        postedBy->{
+          _id,
+          userName,
+          image
         },
-    },
-}`
+      },
+      comments[]{
+        comment,
+        _key,
+        postedBy->{
+          _id,
+          userName,
+          image
+        },
+      }
+    }`;
+    return query;
+  };
+  
+  export const pinDetailMorePinQuery = (pin) => {
+    const query = `*[_type == "pin" && category == '${pin.category}' && _id != '${pin._id}' ]{
+      image{
+        asset->{
+          url
+        }
+      },
+      _id,
+      destination,
+      postedBy->{
+        _id,
+        userName,
+        image
+      },
+      save[]{
+        _key,
+        postedBy->{
+          _id,
+          userName,
+          image
+        },
+      },
+    }`;
+    return query;
+  };
+  
+  export const searchQuery = (searchTerm) => {
+    const query = `*[_type == "pin" && title match '${searchTerm}*' || category match '${searchTerm}*' || about match '${searchTerm}*']{
+          image{
+            asset->{
+              url
+            }
+          },
+              _id,
+              destination,
+              postedBy->{
+                _id,
+                userName,
+                image
+              },
+              save[]{
+                _key,
+                postedBy->{
+                  _id,
+                  userName,
+                  image
+                },
+              },
+            }`;
+    return query;
+  };
+  
+  export const userQuery = (userId) => {
+    const query = `*[_type == "user" && _id == '${userId}']`;
+    return query;
+  };
+  
+  export const userCreatedPinsQuery = (userId) => {
+    const query = `*[ _type == 'pin' && userId == '${userId}'] | order(_createdAt desc){
+      image{
+        asset->{
+          url
+        }
+      },
+      _id,
+      destination,
+      postedBy->{
+        _id,
+        userName,
+        image
+      },
+      save[]{
+        postedBy->{
+          _id,
+          userName,
+          image
+        },
+      },
+    }`;
+    return query;
+  };
+  
+  export const userSavedPinsQuery = (userId) => {
+    const query = `*[_type == 'pin' && '${userId}' in save[].userId ] | order(_createdAt desc) {
+      image{
+        asset->{
+          url
+        }
+      },
+      _id,
+      destination,
+      postedBy->{
+        _id,
+        userName,
+        image
+      },
+      save[]{
+        postedBy->{
+          _id,
+          userName,
+          image
+        },
+      },
+    }`;
+    return query;
+  };
